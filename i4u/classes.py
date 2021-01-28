b3963769fcd6f448fb1ad3601d18df69cabc856e
Ofer Sadan <ofersadan85@gmail.com>
2021-01-28 14:33:50 +0200

    fix client definition in documents
    Signed-off-by: Ofer Sadan <ofersadan85@gmail.com>

--- i4u/classes.py
+++ i4u/classes.py
@@ -110,7 +110,7 @@ class Document:
             self.Items = {'DocumentItem': self.Items}
         if isinstance(self.Payments, list):
             self.Payments = {'Payment': self.Payments}
-        if self.GeneralCustomer is None and self.ClientID is None:
+        if not any((self.ClientID, self.NewCustomer, self.GeneralCustomer)):
             raise ValueError('חייב להופיע זיהוי לקוח או שם לקוח זמני בחשבונית')
 
 
--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
 from pathlib import Path
+
+from setuptools import setup
 
 long_description = Path(__file__).with_name('README.md').read_text(encoding='utf-8')
 
 setup(
     name='i4u',
-    version='0.0.4',
+    version='0.0.5',
     packages=['i4u'],
     url='https://github.com/ofersadan85/i4u-py',
     license='MIT License',
