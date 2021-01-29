afb545c1c65e144f88bdbaa1421d5ca80c8e68cb
Ofer Sadan <ofersadan85@gmail.com>
2021-01-30 00:56:52 +0200

    try adding python 3.6 build
    Signed-off-by: Ofer Sadan <ofersadan85@gmail.com>

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
+import sys
 from pathlib import Path
 
 from setuptools import setup
 
 long_description = Path(__file__).with_name('README.md').read_text(encoding='utf-8')
 
+dataclasses = ['dataclasses>=0.8'] if sys.version.startswith('3.6') else []
+requirements = Path(__file__).with_name('requirements.txt').read_text().split() + dataclasses
+
 setup(
     name='i4u',
     version='0.0.5',
     packages=['i4u'],
-    url='https://github.com/ofersadan85/i4u-py',
+    url='https://github.com/ofersadan85/i4u',
     license='MIT License',
     author='Ofer Sadan',
     author_email='ofersadan85@gmail.com',
@@ -18,10 +22,11 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
     ],
     include_package_data=True,
-    install_requires=["requests>=2.25.1", "zeep>=4.0.0"],
+    install_requires=requirements,
 )
